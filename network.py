2993f97a6d7b2db03a215cbfca889872cf2d5bda
fjaviergb <fco.javier.gb1@gmail.com>
2020-07-10 10:15:29 +0200

    Pickle generalizado

--- network.py
+++ network.py
@@ -1,27 +1,24 @@
 import socket
+import pickle
 
 
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.0.74"
+        self.server = "192.168.1.42"
         self.port = 5555
         self.addr = (self.server, self.port)
-        self.pos = self.connect()
-
-    def getPos(self):
-        return self.pos
+        self.p = self.connect()
 
     def connect(self):
         try:
             self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
         except:
             pass
 
     def send(self, data):
         try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
         except socket.error as e:
             print(e)
