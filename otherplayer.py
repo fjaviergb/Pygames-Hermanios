e2b76f64b688f823c05a6fa077c240e9eb67d528
fjaviergb <fco.javier.gb1@gmail.com>
2020-07-13 17:23:36 +0200

    Clase con métodos y atributos justos para función sprite.draw()

---
+++ otherplayer.py
@@ -0,0 +1,81 @@
+import pygame as pg
+import math
+import numpy as np
+
+###########################################################
+# CLASE
+###########################################################
+
+class otherbody(pg.sprite.Sprite):
+    def __init__(self, x, y, angle, anglehit):
+        super().__init__()
+        self.radio = 20
+        self.x = x
+        self.y = y
+        self.image = pg.Surface((50,50))
+        pg.draw.circle(self.image, (RED), (25,25), self.radio)
+        self.rect = self.image.get_rect(center = (self.x,self.y))
+        self.image.set_colorkey(BLACK)    
+        self.image_orig = self.image
+        self.angle = angle
+        self.anglehit = anglehit
+        self.hitorient = 2
+        
+    def update(self, player):
+        self.image = pg.transform.rotate(self.image_orig, self.angle)
+        self.image.set_colorkey(BLACK)
+        self.rect = self.image.get_rect(center = (self.x, self.y))
+        
+    class otherRhand(pg.sprite.Sprite):
+        def __init__(self):
+            super().__init__()
+            self.radio = 10
+            self.image = pg.Surface((50,50))
+            self.image.set_colorkey(BLACK)
+            pg.draw.circle(self.image, (GREEN), (40,10), self.radio)
+            self.rect = self.image.get_rect(center = (150,150))
+            self.image_orig = self.image
+            
+        def update(self, player):
+            self.image = pg.transform.rotate(self.image_orig, player.anglehit)
+            self.image.set_colorkey(BLACK)
+            self.rect = self.image.get_rect(center = player.rect.center)
+    
+    class otherLhand(pg.sprite.Sprite):
+        def __init__(self):
+            super().__init__()
+            self.radio = 10
+            self.image = pg.Surface((50,50))
+            self.image.set_colorkey(BLACK)            
+            pg.draw.circle(self.image, (BLUE), (10,10), self.radio)
+            self.rect = self.image.get_rect(center = (150,150))
+            self.image_orig = self.image            
+            
+        def update(self, player):
+            self.image = pg.transform.rotate(self.image_orig, player.angle)
+            self.image.set_colorkey(BLACK)            
+            self.rect = self.image.get_rect(center = player.rect.center)
+    
+    class othersword(pg.sprite.Sprite):
+        def __init__(self):
+            super().__init__()
+            self.image = pg.Surface((50,120))
+            pg.draw.rect(self.image, RED, (40,0,5,50))
+            self.rect = self.image.get_rect(center = (150,150))
+            self.image.set_colorkey(BLACK)        
+            self.image_orig = self.image
+
+        def update(self, player):
+            self.image = pg.transform.rotate(self.image_orig, player.anglehit)
+            self.image.set_colorkey(BLACK)            
+            self.rect = self.image.get_rect(center = player.rect.center)
+
+            
+CBASE = (255,255,255)
+CPLAYER = (255,228,181)
+BLACK = (0,0,0)
+GREEN = (0,255,0)
+RED = (255,0,0)
+BLUE = (0,0,255)
+GOLD = (255,215,0)
+WHITE = (255,255,255)
