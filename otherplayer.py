62b2e2e14d457678fdc10f08a2fcb67f6a4234e2
fjaviergb <fco.javier.gb1@gmail.com>
2020-07-14 18:53:01 +0200

    Versión anterior; vida añadida (DEFECTOS)

--- otherplayer.py
+++ otherplayer.py
@@ -7,7 +7,7 @@ import numpy as np
 ###########################################################
 
 class otherbody(pg.sprite.Sprite):
-    def __init__(self, x, y, angle, anglehit):
+    def __init__(self, x, y, angle, anglehit, slashright, slashleft):
         super().__init__()
         self.radio = 20
         self.x = x
@@ -19,12 +19,15 @@ class otherbody(pg.sprite.Sprite):
         self.image_orig = self.image
         self.angle = angle
         self.anglehit = anglehit
-        self.hitorient = 2
+        self.mask = pg.mask.from_surface(self.image) 
+        self.slashright = slashright
+        self.slashleft = slashleft        
         
     def update(self, player):
         self.image = pg.transform.rotate(self.image_orig, self.angle)
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect(center = (self.x, self.y))
+        self.mask = pg.mask.from_surface(self.image)                  
         
     class otherRhand(pg.sprite.Sprite):
         def __init__(self):
@@ -35,11 +38,13 @@ class otherbody(pg.sprite.Sprite):
             pg.draw.circle(self.image, (GREEN), (40,10), self.radio)
             self.rect = self.image.get_rect(center = (150,150))
             self.image_orig = self.image
+            self.mask = pg.mask.from_surface(self.image)                  
             
         def update(self, player):
             self.image = pg.transform.rotate(self.image_orig, player.anglehit)
             self.image.set_colorkey(BLACK)
             self.rect = self.image.get_rect(center = player.rect.center)
+            self.mask = pg.mask.from_surface(self.image)                  
     
     class otherLhand(pg.sprite.Sprite):
         def __init__(self):
@@ -50,11 +55,13 @@ class otherbody(pg.sprite.Sprite):
             pg.draw.circle(self.image, (BLUE), (10,10), self.radio)
             self.rect = self.image.get_rect(center = (150,150))
             self.image_orig = self.image            
+            self.mask = pg.mask.from_surface(self.image)                  
             
         def update(self, player):
             self.image = pg.transform.rotate(self.image_orig, player.angle)
             self.image.set_colorkey(BLACK)            
             self.rect = self.image.get_rect(center = player.rect.center)
+            self.mask = pg.mask.from_surface(self.image)                  
     
     class othersword(pg.sprite.Sprite):
         def __init__(self):
@@ -64,11 +71,13 @@ class otherbody(pg.sprite.Sprite):
             self.rect = self.image.get_rect(center = (150,150))
             self.image.set_colorkey(BLACK)        
             self.image_orig = self.image
+            self.mask = pg.mask.from_surface(self.image)                  
 
         def update(self, player):
             self.image = pg.transform.rotate(self.image_orig, player.anglehit)
             self.image.set_colorkey(BLACK)            
             self.rect = self.image.get_rect(center = player.rect.center)
+            self.mask = pg.mask.from_surface(self.image)                  
 
             
 CBASE = (255,255,255)
