8a28b041bfeda67d8ca03c9b9ec1dd3fec08e9bf
fjaviergb <fco.javier.gb1@gmail.com>
2020-07-17 14:12:13 +0200

    Versión anterior; environment añadido

--- client.py
+++ client.py
@@ -2,13 +2,14 @@ import pygame as pg
 from network import Network
 from SuperPlayer import body
 from otherplayer import otherbody
+import otherenvironment as env
 
 width = 500
 height = 500
 win = pg.display.set_mode((width, height))
 pg.display.set_caption("Client")
 
-def colision(p, sword, po):
+def colision(p, sword):
     if ((p.slashleft and not p.swingleft and not p.backleft) or (p.slashright and not p.swingright and not p.backright) or (p.chargecount == 31 and p.swingleft)):
         block_hit_list = pg.sprite.spritecollide(sword, p.col_sprites, False)
         block_hit_list_masked = pg.sprite.spritecollide(sword, block_hit_list, False, pg.sprite.collide_mask)
@@ -55,37 +56,58 @@ def main():
                 run = False
                 pg.quit()
 
-        pothers = n.send((p.x,p.y,p.angle,p.anglehit,p.slashright,p.slashleft, p.live, p.chargecount))
-            
+        pothers = n.send((p.tipo,p.x,p.y,p.angle,p.anglehit,p.slashright,p.slashleft, p.live, p.chargecount))
+
         win.fill((255,255,255))
         
         for i in pothers:        
-            if (p.x,p.y) != (i[0],i[1]) and i[6] > 0:
-                po = otherbody(i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7])
+            if type(i) != list:
+                if (p.x,p.y) != (i[1],i[2]) and i[7] > 0:
+                    po = otherbody(i[0],i[1],i[2],i[3],i[4],i[5],i[6],i[7],i[8])
+    
+                    p.other_sprites.add(po)
+                    p.other_sprites.add(po.Rhand)
+                    p.other_sprites.add(po.Lhand)
+                    p.other_sprites.add(po.espada) 
+                    p.other_sprites.update(po)
+                    p.other_sprites.draw(win)
+                    p.enem_sword.add(po.espada)
+    
+                    p.col_sprites.add(po)                
+    
+                    if i[8] == 31 or i[6] or i[5]:
+                        p.col_sprites.add(po.espada)                
+                    
+                    if i[5] or i[6]:
+                        block_hit_list = pg.sprite.spritecollide(p, p.enem_sword, False)
+                        block_hit_list_masked = pg.sprite.spritecollide(p, block_hit_list, False, pg.sprite.collide_mask)
+                        if len(block_hit_list_masked) != 0:
+                            p.live -= 1
+                        p.enem_sword = pg.sprite.Group()
+                    
+                    colision(p, p.espada)
+    
+                    p.other_sprites = pg.sprite.Group()   
+        
+            else:
+                for j in i:
+                    if j[0] == 4:
+                        envir = env.circle_obstacle(j[1],j[2])   
+                        p.env_sprites.add(envir)
+                    elif j[0] == 3:
+                        envir = env.rect_obstacle(j[1],j[2])   
+                        p.env_sprites.add(envir)
+                    elif j[0] == 2: 
+                        envir = env.gir_obstacle(j[1],j[2])   
+                        p.env_sprites.add(envir)
+        
+                    p.env_sprites.update()
+                    p.env_sprites.draw(win)
+                    p.col_sprites.add(envir)                
+                    
+                    colision(p, p.espada)
 
-                p.other_sprites.add(po)
-                p.other_sprites.add(po.Rhand)
-                p.other_sprites.add(po.Lhand)
-                p.other_sprites.add(po.espada) 
-                p.other_sprites.update(po)
-                p.other_sprites.draw(win)
-                p.enem_sword.add(po.espada)
-
-                p.col_sprites.add(po)                
-
-                if i[7] == 31 or i[5] or i[4]:
-                    p.col_sprites.add(po.espada)                
-                
-                if i[4] or i[5]:
-                    block_hit_list = pg.sprite.spritecollide(p, p.enem_sword, False)
-                    block_hit_list_masked = pg.sprite.spritecollide(p, block_hit_list, False, pg.sprite.collide_mask)
-                    if len(block_hit_list_masked) != 0:
-                        p.live -= 1
-                    p.enem_sword = pg.sprite.Group()
-                
-                colision(p, p.espada, po)
-
-                p.other_sprites = pg.sprite.Group()   
+                p.env_sprites = pg.sprite.Group()
         
         if p.live > 0:           
             all_sprites.update(p)
